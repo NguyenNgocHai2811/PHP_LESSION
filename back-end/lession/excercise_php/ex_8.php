d250c7538c6e0e413911d27abeb60ba6cfb1a25b
NguyenNgocHai@gmail.com <haiproplayer@email.com>
2024-05-24 15:11:56 +0700

    update finished exercise 8

--- back-end/lession/excercise_php/ex_8.php
+++ back-end/lession/excercise_php/ex_8.php
@@ -1,31 +1,78 @@
 <?php
 // xaaydungj hmaf kiểm tra số ngyên chẵn
 
-function check_even($n){
-    if($n%2==0 && $n>0){
-        echo "số chẵn";
-    }
-    else
-    echo "số lẻ";
-}
+// function check_even($n){
+//     if($n%2==0 && $n>0){
+//         echo "số chẵn";
+//     }
+//     else
+//     echo "số lẻ";
+// }
 
-check_even(2);
+// check_even(2);
 // end funtion check-even
 
 
 // tạo funtion trả về true false;
-function check_even2($n){
-    if($n%2==0 && $n>0){
-        return true;
-    }
-    else
-   return false;
-}
+// function check_even2($n){
+//     if($n%2==0 && $n>0){
+//         return true;
+//     }
+//     else
+//    return false;
+// }
 
-if(check_even2(1)){
-    echo "là số chẵn";
-}
-else{
-    echo "là số lẻ";
-}
+// if(check_even2(1)){
+//     echo "là số chẵn";
+// }
+// else{
+//     echo "là số lẻ";
+// }
+
+
+$myList = array(
+    0=>array(
+    'id'=>1,
+    'description'=>"toi la nguoi toi khong phai la cho"),
+    1=>array(
+        'id'=>2,
+        'description'=>"toi la cho toi khog phai la nguoi"
+    ),
+    2=>array(
+        'id'=>3,
+        'description'=>"toi la cho toi khog phai la nguoi"
+    ),
+    3=>array(
+        'id'=>4,
+        'description'=>"toi la cho toi khog phai la nguoi"
+    ),
+    4=>array(
+        'id'=>5,
+        'description'=>"toi la cho toi khog phai la nguoi"
+    ),
+    
+);
+
+//tao funtion lay id cua mot bai bao trong mang
+
+ function get_id_newpapers($list)
+ {
+    $mylisst_curen = func_get_arg(0);
+     $list_get_id = array();
+
+    foreach($mylisst_curen as $item){
+    $list_get_id[] = $item['id'];
+    }
+    return $list_get_id;
+ }
+ //hàm hiển thị mảng
+ function showArray($ar){
+    echo " <pre>";
+    print_r($ar);
+    echo " <pre>";
+ }
+
+ $list_get_id = get_id_newpapers($myList);
+ showArray($list_get_id);
+ 
 ?>
